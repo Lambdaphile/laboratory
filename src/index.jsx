b05c2516eab47d4a98d77bc12889eb9346cd2d4e
genesis-algorithms <genesisalgorithms@gmail.com>
2020-02-11 11:54:13 +0300

    Setup eslint and prettier

---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './line-utilization/index';
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/line-utilization-popup/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const LineUtilizationPopup = () => <h1>Hello</h1>;
+
+export default LineUtilizationPopup;
